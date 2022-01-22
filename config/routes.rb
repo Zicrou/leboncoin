69e26ef223fd41e0ecf590e0ac6490749c0b1c2b
Zicrou <aazseck@gmail.com>
2022-01-22 04:02:56 +0000

    Devise and Petergate is set Well

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', sign_up: 'cmon_let_me_in' }
   root to: 'cars#index'
 end
